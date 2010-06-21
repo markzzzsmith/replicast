4e4dd38368c4c989a1420d5e2eeb1f7009affb5a
Mark Smith <markzzzsmith@yahoo.com.au>
2010-06-21 21:14:49 +0930

    inet_to_inet function, global sock_fd variables, and atexit() socket close

--- replicast.c
+++ replicast.c
@@ -4,11 +4,12 @@
  *
  */
 
+#include <errno.h>
 #include <stdint.h>
-#include <stdio.h> /* remove after testing if necessary */
-#include <stdlib.h> /* remove after testing if necessary */
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <unistd.h> /* sleep() - remove after testing */
+#include <unistd.h>
 
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -16,6 +17,26 @@
 
 #include "global.h"
 
+
+enum {
+	PKT_BUF_SIZE = 0xffff,
+};
+
+
+void close_sockets(void);
+
+void inet_to_inet_mcast(int *inet_in_sock_fd,
+			const struct in_addr mc_group,
+			const unsigned int port,
+			const struct in_addr in_intf_addr,
+			int *inet_out_sock_fd,
+			const int mc_ttl,
+			const int mc_loop,
+			const struct in_addr out_intf_addr,
+			const struct sockaddr_in inet_mc_dests[],
+			const unsigned int mc_dests_num);
+
+void exit_errno(const char *func_name, const unsigned int linenum, int errnum);
 
 int open_inet_rx_mc_sock(const struct in_addr mc_group,
 			 const unsigned int port,
@@ -30,13 +51,13 @@ int open_inet6_rx_mc_sock(const struct in6_addr mc_group,
 void close_inet6_rx_mc_sock(const int sock_fd);
 
 int open_inet_tx_mc_sock(const int mc_ttl,
-			 const int no_mc_loop,
+			 const int mc_loop,
 			 const struct in_addr out_intf_addr);
 
 void close_inet_tx_mc_sock(int sock_fd);
 
 int open_inet6_tx_mc_sock(const int mc_hops,
-			  const int no_mc_loop,
+			  const int mc_loop,
 			  const unsigned int out_intf_idx);
 
 void close_inet6_tx_mc_sock(int sock_fd);
@@ -44,63 +65,117 @@ void close_inet6_tx_mc_sock(int sock_fd);
 int inet_tx_mcast(const int sock_fd,
 		  const void *pkt,
 		  const size_t pkt_len,
- 		  const struct sockaddr_in group_dests[],
-		  const unsigned int group_dests_num);
+ 		  const struct sockaddr_in inet_mc_dests[],
+		  const unsigned int mc_dests_num);
 
 int inet6_tx_mcast(const int sock_fd,
 		   const void *pkt,
 		   const size_t pkt_len,
- 		   const struct sockaddr_in6 group_dests[],
-		   const unsigned int group_dests_num);
+ 		   const struct sockaddr_in6 inet6_mc_dests[],
+		   const unsigned int mc_dests_num);
 
+int inet_in_sock_fd = -1;
+int inet6_in_sock_fd = -1;
+int inet_out_sock_fd = -1;
+int inet6_out_sock_fd = -1;
 
 int main(int argc, char *argv[])
 {
-	int in_sock_fd;
-	int out_sock_fd;
-	//struct in6_addr inet6_mcaddr;
-	struct in_addr inet_mcaddr;
+	struct in_addr rx_mcaddr;
 	struct in_addr in_intf_addr;
 	struct in_addr out_intf_addr;
-	struct sockaddr_in inet_out_mcaddr;
-	const unsigned int pkt_buf_size = 0xffff;
-	uint8_t pkt_buf[pkt_buf_size];
-	unsigned int rx_pkt_len;
+	struct sockaddr_in inet_mc_dests[3];
+	
 
+	atexit(close_sockets);
 
-	/* IPv6 */
-	//inet_pton(AF_INET6, "ff05::5", &inet6_mcaddr);
-	//in_sock_fd = open_inet6_rx_mc_sock(inet6_mcaddr, 1234, 9); 
-
-	/* IPv4 */
-	inet_pton(AF_INET, "224.4.4.4", &inet_mcaddr);
+	inet_pton(AF_INET, "224.4.4.4", &rx_mcaddr);
 	inet_pton(AF_INET, "1.1.1.1", &in_intf_addr);
-	in_sock_fd = open_inet_rx_mc_sock(inet_mcaddr, 1234, in_intf_addr);
-	if (in_sock_fd == -1) {
-		perror("");
-	}
 
 	inet_pton(AF_INET, "1.1.1.1", &out_intf_addr);
-	out_sock_fd = open_inet_tx_mc_sock(0, 0, out_intf_addr);
 
-	memset(&inet_out_mcaddr, 0, sizeof(inet_out_mcaddr));
-	inet_out_mcaddr.sin_family = AF_INET;
-	inet_pton(AF_INET, "224.5.5.5", &inet_out_mcaddr.sin_addr);
-	inet_out_mcaddr.sin_port = htons(1234);
+	memset(&inet_mc_dests[0], 0, sizeof(inet_mc_dests[0]));
+	inet_mc_dests[0].sin_family = AF_INET;
+	inet_pton(AF_INET, "224.5.5.5", &inet_mc_dests[0].sin_addr);
+	inet_mc_dests[0].sin_port = htons(1234);
+	
+	memset(&inet_mc_dests[1], 0, sizeof(inet_mc_dests[1]));
+	inet_mc_dests[1].sin_family = AF_INET;
+	inet_pton(AF_INET, "224.6.6.6", &inet_mc_dests[1].sin_addr);
+	inet_mc_dests[1].sin_port = htons(1234);
+	
+	memset(&inet_mc_dests[2], 0, sizeof(inet_mc_dests[2]));
+	inet_mc_dests[2].sin_family = AF_INET;
+	inet_pton(AF_INET, "224.7.7.7", &inet_mc_dests[2].sin_addr);
+	inet_mc_dests[2].sin_port = htons(1234);
+	
+	inet_to_inet_mcast(&inet_in_sock_fd, rx_mcaddr, 1234, in_intf_addr,
+		&inet_out_sock_fd, 1, 0, out_intf_addr, inet_mc_dests, 3);
+
+	return 0;
+
+}
+
+
+void close_sockets(void)
+{
+
+
+	close_inet_rx_mc_sock(inet_in_sock_fd);
+	close_inet6_rx_mc_sock(inet6_in_sock_fd);
+	close_inet_tx_mc_sock(inet_out_sock_fd);
+	close_inet6_tx_mc_sock(inet6_out_sock_fd);
+
+}
+
+
+void inet_to_inet_mcast(int *inet_in_sock_fd,
+			const struct in_addr mc_group,
+			const unsigned int port,
+			const struct in_addr in_intf_addr,
+			int *inet_out_sock_fd,
+			const int mc_ttl,
+			const int mc_loop,
+			const struct in_addr out_intf_addr,
+			const struct sockaddr_in inet_mc_dests[],
+			const unsigned int mc_dests_num)
+{
+	uint8_t pkt_buf[PKT_BUF_SIZE];
+	ssize_t rx_pkt_len;
+
+
+	*inet_in_sock_fd = open_inet_rx_mc_sock(mc_group, port, in_intf_addr);
+	if (*inet_in_sock_fd == -1) {
+		exit_errno(__func__, __LINE__, errno);
+	}
+
+	*inet_out_sock_fd = open_inet_tx_mc_sock(mc_ttl, mc_loop,
+		out_intf_addr);
+	if (*inet_out_sock_fd == -1) {
+		exit_errno(__func__, __LINE__, errno);
+	}
 
 	for ( ;; ) {
-		rx_pkt_len = recv(in_sock_fd, pkt_buf, pkt_buf_size, 0);	
+		rx_pkt_len = recv(*inet_in_sock_fd, pkt_buf, PKT_BUF_SIZE, 0);
 		if (rx_pkt_len > 0) {
-			inet_tx_mcast(out_sock_fd, pkt_buf, rx_pkt_len,
-				&inet_out_mcaddr, 1);
+			inet_tx_mcast(*inet_out_sock_fd, pkt_buf, rx_pkt_len,
+				inet_mc_dests, mc_dests_num);
+		} else if (rx_pkt_len == -1) {
+			exit_errno(__func__, __LINE__, errno);
 		}
 	}
 
-	/* IPv4 */
-	close_inet_rx_mc_sock(in_sock_fd);
-	close_inet_tx_mc_sock(out_sock_fd);
 
-	return 0;
+}
+
+
+void exit_errno(const char *func_name, const unsigned int linenum, int errnum)
+{
+
+
+	fprintf(stderr, "%s():%d error: %s\n", func_name, linenum,
+		strerror(errnum));
+	exit(EXIT_FAILURE);
 
 }
 
@@ -150,7 +225,9 @@ void close_inet_rx_mc_sock(const int sock_fd)
 {
 
 
-	close(sock_fd);
+	if (sock_fd != -1) {
+		close(sock_fd);
+	}
 
 }
 
@@ -200,18 +277,20 @@ void close_inet6_rx_mc_sock(const int sock_fd)
 {
 
 
-	close(sock_fd);
+	if (sock_fd != -1) {
+		close(sock_fd);
+	}
 
 }
 
 
 int open_inet_tx_mc_sock(const int mc_ttl,
-			 const int no_mc_loop,
+			 const int mc_loop,
 			 const struct in_addr out_intf_addr)
 {
 	int sock_fd;
 	uint8_t ttl;
-	uint8_t mc_loop;
+	uint8_t mcloop;
 	int ret;
 
 
@@ -229,13 +308,15 @@ int open_inet_tx_mc_sock(const int mc_ttl,
 		}
 	}
 
-	if (no_mc_loop == 1) {
-		mc_loop = 0;
-		ret = setsockopt(sock_fd, IPPROTO_IP, IP_MULTICAST_LOOP,
-			&mc_loop, sizeof(mc_loop));
-		if (ret == -1) {
-			return -1;
-		}
+	if (mc_loop == 1) {
+		mcloop = 1;
+	} else {
+		mcloop = 0;
+	}
+	ret = setsockopt(sock_fd, IPPROTO_IP, IP_MULTICAST_LOOP,
+		&mc_loop, sizeof(mc_loop));
+	if (ret == -1) {
+		return -1;
 	}
 
 	ret = setsockopt(sock_fd, IPPROTO_IP, IP_MULTICAST_IF, &out_intf_addr,
@@ -253,18 +334,20 @@ void close_inet_tx_mc_sock(int sock_fd)
 {
 
 
-	close(sock_fd);
+	if (sock_fd != -1) {
+		close(sock_fd);
+	}
 
 }
 
 
 int open_inet6_tx_mc_sock(const int mc_hops,
-			  const int no_mc_loop,
+			  const int mc_loop,
 			  const unsigned int out_intf_idx)
 {
 	int sock_fd;
 	uint8_t hops;
-	uint8_t mc_loop;
+	uint8_t mcloop;
 	uint16_t out_intf;
 	int ret;
 	
@@ -284,13 +367,15 @@ int open_inet6_tx_mc_sock(const int mc_hops,
 		}
 	}
 
-	if (no_mc_loop == 1) {
-		mc_loop = 0;
-		ret = setsockopt(sock_fd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP,
-			&mc_loop, sizeof(mc_loop));
-		if (ret == -1) {
-			return -1;
-		}
+	if (mc_loop == 1) {
+		mcloop = 1;
+	} else {
+		mcloop = 0;
+	}
+	ret = setsockopt(sock_fd, IPPROTO_IPV6, IPV6_MULTICAST_LOOP,
+		&mc_loop, sizeof(mc_loop));
+	if (ret == -1) {
+		return -1;
 	}
 
 	out_intf = out_intf_idx;
@@ -309,7 +394,9 @@ void close_inet6_tx_mc_sock(int sock_fd)
 {
 
 
-	close(sock_fd);
+	if (sock_fd != -1) {
+		close(sock_fd);
+	}
 
 }
 
@@ -317,17 +404,17 @@ void close_inet6_tx_mc_sock(int sock_fd)
 int inet_tx_mcast(const int sock_fd,
 		  const void *pkt,
 		  const size_t pkt_len,
- 		  const struct sockaddr_in group_dests[],
-		  const unsigned int group_dests_num)
+ 		  const struct sockaddr_in inet_mc_dests[],
+		  const unsigned int mc_dests_num)
 {
 	unsigned int i;
 	unsigned int tx_success = 0;
 	ssize_t ret;
 
 
-	for (i = 0; i < group_dests_num; i++) {
+	for (i = 0; i < mc_dests_num; i++) {
 		ret = sendto(sock_fd, pkt, pkt_len, 0, 	
-			(struct sockaddr *)&group_dests[i],
+			(struct sockaddr *)&inet_mc_dests[i],
 			sizeof(struct sockaddr_in));
 		if (ret != -1) {
 			tx_success++;
@@ -342,17 +429,17 @@ int inet_tx_mcast(const int sock_fd,
 int inet6_tx_mcast(const int sock_fd,
 		   const void *pkt,
 		   const size_t pkt_len,
- 		   const struct sockaddr_in6 group_dests[],
-		   const unsigned int group_dests_num)
+ 		   const struct sockaddr_in6 inet6_mc_dests[],
+		   const unsigned int mc_dests_num)
 {
 	unsigned int i;
 	unsigned int tx_success = 0;
 	ssize_t ret;
 
 
-	for (i = 0; i < group_dests_num; i++) {
+	for (i = 0; i < mc_dests_num; i++) {
 		ret = sendto(sock_fd, pkt, pkt_len, 0, 	
-			(struct sockaddr *)&group_dests[i],
+			(struct sockaddr *)&inet6_mc_dests[i],
 			sizeof(struct sockaddr_in6));
 		if (ret != -1) {
 			tx_success++;
