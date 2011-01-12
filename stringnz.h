a1fd1637d3f3900500a8de9a03b919e00bc3ea6b
Mark Smith <markzzzsmith@yahoo.com.au>
2011-01-12 23:00:36 +1030

    delete unnecessary strnchr and strnrchr

--- stringnz.h
+++ stringnz.h
@@ -4,8 +4,4 @@
 
 char *strnzcpy(char *dest, const char *src, const size_t n);
 
-char *strnchr(const char *str, const int c, const size_t n);
-
-char *strnrchr(const char *str, const int c, const size_t n);
-
 #endif /* __STRINGNZ_H */
